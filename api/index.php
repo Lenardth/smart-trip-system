2ce8355cd986995a0e8cc4847ac6ca2af1213a64
Lenardth <98667180+Lenardth@users.noreply.github.com>
2026-03-24 22:48:32 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,18 +1,11 @@
-{
-  "buildCommand": "composer install --no-dev --optimize-autoloader && npm run build",
-  "functions": {
-    "api/index.php": {
-      "runtime": "vercel-php@0.7.2"
-    }
-  },
-  "routes": [
-    {
-      "src": "/build/(.*)",
-      "dest": "/public/build/$1"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "/api/index.php"
-    }
-  ]
+<?php
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+if (!file_exists('/tmp/database.sqlite')) {
+    touch('/tmp/database.sqlite');
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
 }
+
+require __DIR__ . '/../public/index.php';
