ce6f3baee839a00442131cdda94e32f24bba9e7d
Lenardth <98667180+Lenardth@users.noreply.github.com>
2026-03-24 23:16:57 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,23 +1,29 @@
 <?php
 
 ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-define('LARAVEL_START', microtime(true));
+try {
+    define('LARAVEL_START', microtime(true));
 
-require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 
-$app = require __DIR__ . '/../bootstrap/app.php';
+    $app = require __DIR__ . '/../bootstrap/app.php';
 
-if (!file_exists('/tmp/database.sqlite')) {
-    touch('/tmp/database.sqlite');
+    if (!file_exists('/tmp/database.sqlite')) {
+        touch('/tmp/database.sqlite');
+    }
+
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    )->send();
+    $kernel->terminate($request, $response);
+
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo '<pre>';
+    echo $e->getMessage() . "\n\n";
+    echo $e->getTraceAsString();
+    echo '</pre>';
 }
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
