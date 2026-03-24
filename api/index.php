af3e40ab88457718687dcef35dd6126386e0c660
Lenardth <98667180+Lenardth@users.noreply.github.com>
2026-03-24 22:00:25 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+if (!file_exists('/tmp/database.sqlite')) {
+    touch('/tmp/database.sqlite');
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+}
+
 require __DIR__ . '/../public/index.php';
