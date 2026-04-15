a483d31e8e3fc038c5150228d9eba32a4966bc6d
Hlabangwana Lenard Tivanani <98667180+Lenardth@users.noreply.github.com>
2026-04-15 11:50:32 +0200

    fix: remove https force on local env, add auth CSS imports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,7 @@ class AppServiceProvider extends ServiceProvider
     
     public function boot(): void
     {
-        
-        if ($this->app->environment('production') ||
-            (config('app.url') && strpos(config('app.url'), 'https') !== false)) {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
